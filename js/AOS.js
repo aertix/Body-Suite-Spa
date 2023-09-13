9237ae904c23e724d0b0bb8c2c167ef6090ddc53
Aertix <hawk.sj717@gmail.com>
2023-09-12 17:29:52 -0700

    edits

--- js/AOS.js
+++ js/AOS.js
@@ -1,5 +1,5 @@
 AOS.init({
     offset: 300, // offset (in px) from the original trigger point
     delay: 50, // values from 0 to 3000, with step 50ms
-    duration: 1000 // values from 0 to 3000, with step 50ms
+    duration: 900 // values from 0 to 3000, with step 50ms
 });
