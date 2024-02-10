c4cb91760ddeada12153ebee6237f524a14221ae
Shawn Jerry <101995383+aertix@users.noreply.github.com>
2024-02-10 12:30:04 -0800

    mobilenav part 2

--- js/general.js
+++ js/general.js
@@ -25,30 +25,30 @@ arrow.addEventListener('click', () => {
 
 })
 
-arrow.addEventListener('click', () => {
+// arrow.addEventListener('click', () => {
 
-    console.log(toggle)
+//     console.log(toggle)
 
-    if (toggle === 1) {
-        arrow.classList.add('leftTurn');
-        arrow.classList.remove('rightTurn');
-        document.querySelector('.dropdownextended').classList.add('appear');
-        document.querySelector('.dropdownextended').classList.remove('disappear');
-        document.querySelector('.dropdownextended').style.display = 'flex';
+//     if (toggle === 1) {
+//         arrow.classList.add('leftTurn');
+//         arrow.classList.remove('rightTurn');
+//         document.querySelector('.dropdownextended').classList.add('appear');
+//         document.querySelector('.dropdownextended').classList.remove('disappear');
+//         document.querySelector('.dropdownextended').style.display = 'flex';
 
-    }
-    else if (toggle === -1) {
-        arrow.classList.add('rightTurn');
-        arrow.classList.remove('leftTurn');
-        document.querySelector('.dropdownextended').classList.add('disappear');
-        document.querySelector('.dropdownextended').classList.remove('appear');
-        setTimeout(() => {
-            document.querySelector('.dropdownextended').style.display = 'none';
-        }, 500)
-    }
-    toggle *= -1;
+//     }
+//     else if (toggle === -1) {
+//         arrow.classList.add('rightTurn');
+//         arrow.classList.remove('leftTurn');
+//         document.querySelector('.dropdownextended').classList.add('disappear');
+//         document.querySelector('.dropdownextended').classList.remove('appear');
+//         setTimeout(() => {
+//             document.querySelector('.dropdownextended').style.display = 'none';
+//         }, 500)
+//     }
+//     toggle *= -1;
 
-})
+// })
 
 let toggle2 = 1
 const exit = document.querySelector('.exit');
