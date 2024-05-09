49e3a9e684a91547c4ab6737ffe9ae3a16cc100d
Shawn Jerry <101995383+aertix@users.noreply.github.com>
2024-05-08 19:35:00 -0700

    gift cards

--- js/general.js
+++ js/general.js
@@ -5,16 +5,13 @@ arrow.addEventListener('click', () => {
     console.log(toggle)
 
     if (toggle === 1) {
-        arrow.classList.add('leftTurn');
-        arrow.classList.remove('rightTurn');
         document.querySelector('.dropdownextended').classList.add('appear');
         document.querySelector('.dropdownextended').classList.remove('disappear');
         document.querySelector('.dropdownextended').style.display = 'flex';
 
     }
     else if (toggle === -1) {
-        arrow.classList.add('rightTurn');
-        arrow.classList.remove('leftTurn');
+
         document.querySelector('.dropdownextended').classList.add('disappear');
         document.querySelector('.dropdownextended').classList.remove('appear');
         setTimeout(() => {
